2370f363f7812d6a2affb4470641d1dd09fef79c
julien <julien@OldDeni>
2017-03-22 17:11:38 +0100

    Opening

--- src/main/java/com/imagejfxTraining/imagebrowser/DefaultFileService.java
+++ src/main/java/com/imagejfxTraining/imagebrowser/DefaultFileService.java
@@ -6,34 +6,74 @@
 package com.imagejfxTraining.imagebrowser;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import org.scijava.SciJava;
+import java.util.stream.Collectors;
+import javax.imageio.ImageIO;
 import org.scijava.plugin.Plugin;
 import org.scijava.service.AbstractService;
+import org.scijava.service.SciJavaService;
 
 /**
  *
  * @author julien
  */
-@Plugin(type=SciJava.class, priority=10)
+@Plugin(type=SciJavaService.class, priority=10)
 public class DefaultFileService extends AbstractService implements FileService {
     
     
-    private List <Files> filesList;
+    private final List <ItemFile> filesList = new ArrayList<>();
     
     @Override
-    public List<Files> getFilesList() {
+    public List<ItemFile> getFilesList() {
         return filesList;
     }
     
+    @Override
     public void updateData(String dirName){
         
         filesList.clear();
         File dir = new File(dirName);
-        File[] filesList = dir.listFiles();
-        for (File file : filesList) {
-            System.out.println("");
+        File[] listOfFiles = dir.listFiles();
+        for (File file : listOfFiles) {
+            ItemFile item = null;
+            if (file.isDirectory())
+                    item = new Directory(file.getPath());
+            else if (isImage(file))
+                    item = new ImageFile (file.getPath());
+            if (item != null)
+                filesList.add(item);
         }
     }
     
+    
+    
+    public static boolean isImage(File file) {
+        try {
+            ImageIO.read(file);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+        
+    }    
+
+    @Override
+    public void open(String fileName) {
+        filesList.stream()
+                .filter((file) -> (file.getName().equals(fileName)))
+                .forEachOrdered((file) -> {
+                    file.open();
+        });
+    
+    }
+
+    @Override
+    public void delete(String fileName) {
+        List <ItemFile> toRemove = filesList.stream().
+                filter((f) -> f.getName().equals(fileName))
+                .collect(Collectors.toList());
+        filesList.removeAll(toRemove);
+    }
 }
